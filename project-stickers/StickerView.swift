d5aee867125492f69aea0a6125cc042c9318aa97
Patrick Gatewood <pg8wood@vt.edu>
2019-07-31 14:33:54 -0400

    first commit

---
+++ project-stickers/StickerView.swift
@@ -0,0 +1,21 @@
+//
+//  StickerView.swift
+//  project-stickers
+//
+//  Created by Patrick Gatewood on 7/31/19.
+//  Copyright © 2019 Patrick Gatewood. All rights reserved.
+//
+
+import UIKit
+
+class StickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
