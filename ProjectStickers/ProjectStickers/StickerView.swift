ddf2ba2f054f83a439faecb4a925f7c6cde299ef
Patrick Gatewood <=>
2019-09-03 15:14:52 -0400

    Add date labels

--- ProjectStickers/ProjectStickers/StickerView.swift
+++ ProjectStickers/ProjectStickers/StickerView.swift
@@ -8,22 +8,22 @@
 
 import SwiftUI
 
-var outlineColors: [Color] = [.blue, .red, .blue]
+let outlineColors: [Color] = [.blue, .red, .white]
 
 var outlineWidth: CGFloat = 6.0
 var badgeFeatureWidth: CGFloat = 40.0
 
 // Not super pretty, but since SwiftUI draws outlines outward from the Shape's bounding rect,
 // we must calculate the padding when using strokes of different thickness
+var totalOutlinePadding: CGFloat {
+    outlineWidth * CGFloat(integerLiteral: outlineColors.count) - (outlineWidth / 2)
+}
+
 var badgeFeaturePadding: CGFloat {
-    let totalOutlinePadding =
-        outlineWidth * CGFloat(integerLiteral: outlineColors.count) - (outlineWidth / 2)
-    
     return totalOutlinePadding + (badgeFeatureWidth / 2)
 }
 
 struct Badge : View {
-
     var body: some View {
         ZStack {
             ForEach((0...outlineColors.count - 1), id: \.self) { index in
@@ -35,6 +35,16 @@ struct Badge : View {
             Circle()
                 .stroke(Color.black, lineWidth: badgeFeatureWidth)
                 .padding(badgeFeaturePadding)
+            
+            HStack {
+                Text("20")
+                    .frame(width: badgeFeatureWidth, height: 30, alignment: .center)
+                Spacer()
+                Text("19")
+                    .frame(width: badgeFeatureWidth, height: nil, alignment: .center)
+            }
+            .background(Color.white)
+            .padding(totalOutlinePadding)
         }
         .padding(10)
     }
@@ -43,7 +53,19 @@ struct Badge : View {
 #if DEBUG
 struct Badge_Previews : PreviewProvider {
     static var previews: some View {
-        Badge()
+       Group {
+          Badge()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+            .previewDisplayName("iPhone SE")
+
+          Badge()
+            .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
+            .previewDisplayName("iPhone XS Max")
+        
+            Badge()
+                .previewDevice(PreviewDevice(rawValue: "iPad Pro (12.9-inch) (3rd generation)"))
+                .previewDisplayName("iPad Pro (12.9-inch) (3rd generation)")
+       }
     }
 }
 #endif
